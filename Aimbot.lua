b5383007b4a4a41864fe12fea9317bf5782029cf
Quins1231 <spring.husky19@gmail.com>
2025-05-13 00:54:38 +0200

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -185,7 +185,7 @@ local GetClosestPlayer = function()
 	local LockPart = Settings.LockPart
 
 	if not Environment.Locked then
-		RequiredDistance = Environment.FOVSettings.Enabled and Environment.FOVSettings.Radius or 1000
+		RequiredDistance = Environment.FOVSettings.Enabled and Environment.FOVSettings.Radius or 2000
 
 		for _, Value in next, GetPlayers(Players) do
 			local Character = __index(Value, "Character")
